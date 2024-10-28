db9ccfde575e5a8d479e2c6bc54bdade68ebcd83
awitten007@gmail.com <awitten1@terpmail.umd.edu>
2024-10-28 01:13:20 +0000

    random access pattern

--- cgroups/file_memory.cpp
+++ cgroups/file_memory.cpp
@@ -8,46 +8,98 @@
 #include <sys/stat.h>
 #include <thread>
 #include <fmt/core.h>
+#include <iostream>
 
 //
 // apt install libfmt-dev
 // g++ file_memory.cpp -std=c++11 -o file_memory `pkg-config --cflags --libs fmt`
 //
 
-#define PAGE_SIZE 4096
+#define BUF_SIZE 4096
+
+size_t get_file_size(int fd);
+std::string find_arg(int argc, char** argv, std::string arg);
+
+void read_file_sequential(int fd, int bytes_per_second) {
+  int sleep_time_ms = (BUF_SIZE * 1000) / bytes_per_second;
+  char buf[BUF_SIZE];
+
+  for (;;) {
+    int ret = read(fd, buf, BUF_SIZE);
+    if (ret < 0) {
+      throw std::runtime_error{"failed to read from file"};
+    }
+    if (ret == 0) {
+      break;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
+  }
+}
+
+void read_file_random(int fd, int bytes_per_second) {
+  int sleep_time_ms = (BUF_SIZE * 1000) / bytes_per_second;
+
+  char buf[BUF_SIZE];
+  size_t bytes_read = 0;
+  size_t file_sz = get_file_size(fd);
+
+  for (;;) {
+    size_t page_offset = rand() % ((file_sz + BUF_SIZE - 1) / BUF_SIZE);
+    size_t offset = page_offset * BUF_SIZE;
+    int ret = pread(fd, buf, BUF_SIZE, offset);
+    if (ret < 0) {
+      throw std::runtime_error{"failed to read file"};
+    }
+    bytes_read += ret;
+    if (bytes_read > file_sz) {
+      break;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
+  }
+}
 
 int main(int argc, char** argv) {
-  if (argc < 2) {
-    throw std::runtime_error{"must provide filename as first argument"};
+  std::string filename = find_arg(argc, argv, "--filename");
+  std::string access_pattern = find_arg(argc, argv, "--access_pattern");
+  std::string bytes_per_s = find_arg(argc, argv, "--bytes_per_second");
+  if (access_pattern != "sequential" && access_pattern != "random") {
+    throw std::runtime_error{"access pattern must be sequential or random"};
   }
-  std::string filename = std::string(argv[1]);
-  int fd = open(filename.c_str(), O_RDONLY);
 
+  int fd = open(filename.c_str(), O_RDONLY);
   if (fd < 0) {
     throw std::runtime_error{fmt::format("failed to open file {}. reason: {}", filename, strerror(errno))};
   }
 
+  int bytes_per_second = std::stoi(bytes_per_s);
+  std::cout << bytes_per_s << std::endl;
+  if (access_pattern == "sequential") {
+    read_file_sequential(fd, bytes_per_second);
+  } else if (access_pattern == "random") {
+    read_file_random(fd, bytes_per_second);
+  }
+
+  pause();
+  return 0;
+}
+
+
+size_t get_file_size(int fd) {
   struct stat st;
   int ret = fstat(fd, &st);
   if (ret == -1) {
     throw std::runtime_error{fmt::format("failed to stat file. reason: {}", strerror(errno))};
   }
 
-  size_t file_sz = st.st_size;
-  int bytes_per_second = std::stoi(argv[2]);
-  int sleep_time_ms = (PAGE_SIZE * 1000) / bytes_per_second;
-  char buf[PAGE_SIZE];
+  return st.st_size;
+}
 
-  for (;;) {
-    size_t page_offset = rand() % ((file_sz + PAGE_SIZE - 1) / PAGE_SIZE);
-    size_t offset = page_offset * PAGE_SIZE;
-    ret = pread(fd, buf, PAGE_SIZE, offset);
-    if (ret < 0) {
-      throw std::runtime_error{"failed to read file"};
+std::string find_arg(int argc, char** argv, std::string arg) {
+  for (int i = 0; i < argc; ++i) {
+    if (std::string(argv[i]) == arg) {
+      return std::string(*(argv + i + 1));
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
   }
-
-  return 0;
+  throw std::runtime_error{fmt::format("must provide argument {}", arg)};
 }
 
