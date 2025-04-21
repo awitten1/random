7ec61ab337bf1c2a7da64c7b3b5ae26e77545205
Andrew Witten <awitten007@gmail.com>
2025-04-20 23:13:00 -0400

    inline asm

--- asm/x64/main.cpp
+++ asm/x64/main.cpp
@@ -8,5 +8,13 @@ extern "C" int SomeFunction();
 
 int main() {
     cout << "result is " << SomeFunction() << std::endl;
+
+    int i = -1;
+    asm (
+        "mov $56, %0"
+        : "=r" (i)
+    );
+
+    cout << "i is " << i << std::endl;
     return 0;
 }
