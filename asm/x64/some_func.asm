fd291034b25898a34d67a4508502c169d23a1b82
Andrew Witten <awitten007@gmail.com>
2025-05-03 16:53:14 -0400

    print something

--- asm/x64/some_func.asm
+++ asm/x64/some_func.asm
@@ -10,12 +10,35 @@ SomeFunction:
 .global RegTest
 .type RegTest, @function
 
+.data
+    message:  .string "Hello, World!\n"      # Null-terminated string
+    number1:  .word   42                   # 16-bit word
+    bigNum:   .long   1000000              # 32-bit value
+    realNum:  .float  3.14                 # 32-bit floating point
+    myArray:  .zero   10                   # 10 bytes of zeros
+    vector:   .long   1, 2, 3, 4           # Array of 4 longs
+
+.text
 RegTest:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    subq $8, %rsp
+
     movq $-1, %rax
     mov $5, %al
 
     mov $17, %ax
     mov $1, %eax
+
+
+    leaq message(%rip), %rdi
+    xorq %rax, %rax 
+    call printf
+
+    movq %rbp, %rsp
+    popq %rbp
+
     ret
 
 # This fixes assembler warning.
