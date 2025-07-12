b2e1f3b1090b80893e91828f9bd4688c09ea3f5d
Andrew Witten <awitten007@gmail.com>
2025-07-12 19:31:10 -0400

    arrow deps

---
+++ arrow/build-deps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eux
+
+dir=$(dirname $(realpath $0))
+
+apt_installs() {
+    sudo apt update && \
+        sudo apt install -y cmake build-essential ninja-build
+}
+
+build_arrow() {
+    if [ -d $dir/deps-install ]; then
+        return 0
+    fi
+
+    mkdir -p $dir/deps/source
+    mkdir -p $dir/deps/install
+    mkdir -p $dir/deps/build
+    (cd $dir/deps/source && \
+        git clone --branch apache-arrow-20.0.0 https://github.com/apache/arrow )
+
+    pushd $dir/deps
+
+
+    cmake -S $dir/deps/source/arrow/cpp --preset ninja-release-minimal \
+        -B $dir/deps/build/arrow -DCMAKE_INSTALL_PREFIX=$dir/deps/install -G Ninja
+
+    cmake --build $dir/deps/build/arrow -j6
+    cmake --install $dir/deps/build/arrow
+    popd
+
+}
+
+apt_installs
+build_arrow
